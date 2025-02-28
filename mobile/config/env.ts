2b46cd03ddc8e416b0954e364424d2e444edf445
yprite <yprite@gmail.com>
2025-02-28 16:07:00 +0900

    fix: nodejs expo 설정 오류 해결
    - 원인 : nodejs 버젼이 달라서 발생한 문제
    
    Signed-off-by: yprite <yprite@gmail.com>

--- mobile/config/env.ts
+++ mobile/config/env.ts
@@ -1,28 +1,28 @@
 type Environment = {
   apiUrl: string;
-  env: 'development' | 'production';
+  env: 'dev' | 'prd';
 };
 
 const ENV = {
-  development: {
+  dev: {
     apiUrl: 'http://localhost:20010',
-    env: 'development',
+    env: 'dev',
   },
-  production: {
-    apiUrl: 'https://your-production-api.com',  // 실제 프로덕션 URL
-    env: 'production',
+  prd: {
+    apiUrl: 'https://3.36.132.159:20010',  // 실제 프로덕션 URL
+    env: 'prd',
   },
 } as const;
 
 const getEnvVars = (): Environment => {
   // Docker 환경에서는 REACT_APP_ENV 환경변수를 통해 환경 설정
-  const environment = process.env.REACT_APP_ENV || 'development';
+  const environment = process.env.EXPO_PUBLIC_APP_ENV || 'dev';
   
-  if (environment === 'production') {
-    return ENV.production;
+  if (environment === 'prd') {
+    return ENV.prd;
   }
   
-  return ENV.development;
+  return ENV.dev;
 };
 
 export default getEnvVars(); 
